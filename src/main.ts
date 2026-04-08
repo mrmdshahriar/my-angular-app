d04698d7f91ac9382a3daa37e0cd57c403c8bc9a
mrmdshahriar <shahriar.ict@gmail.com>
2026-04-08 10:14:27 +0600

    sdfdsf

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,35 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <button (click)="ok = !ok">Toggle</button>
+    <div *ngIf="ok; else other">OK</div>
+    <ng-template #other>Not OK</ng-template>
+    <ng-container *ngTemplateOutlet="ok ? okTpl : errorTpl"></ng-container>
+
+<ng-template #okTpl>
+  <h2>OK111 ✅</h2>
+</ng-template>
+
+<ng-template #errorTpl>
+  <h2>Error222 ❌</h2>
+</ng-template>
+
+    <ul>
+      <li *ngFor="let item of items; index as i">{{ i }} - {{ item }}</li>
+    </ul>
+  `
+})
+export class App {
+  ok = false;
+  isOk = false;
+  items = ['A','B','C'];
+}
+
+bootstrapApplication(App);
